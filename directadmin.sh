f06ca1c4789197940380918ef6d3dd43a6b301a1
CyberGX <anonymous.aquila@gmail.com>
2018-07-22 12:44:43 +0430

    Change Directadmin

--- directadmin.sh
+++ directadmin.sh
@@ -1,1 +1,13 @@
-cd /usr/local/directadmin/custombuild; ./build update; ./build secure_php
+#!/bin/bash
+
+# Secure PHP & Add Disabled Function
+# cd /usr/local/directadmin/custombuild; ./build update; ./build secure_php
+
+echo "Email : "
+read email
+
+
+sed 's/email@domain.com/$email/g' report.txt
+
+# Regular updates of system services, libraries and scripts.
+#cd /usr/local/directadmin/custombuild; ./build update; ./build all d
