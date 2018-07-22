20550cec2b05087b2b311adb8f7b4c37904f53f6
CyberGX <anonymous.aquila@gmail.com>
2018-07-22 12:48:39 +0430

    .

--- directadmin.sh
+++ directadmin.sh
@@ -3,11 +3,9 @@
 # Secure PHP & Add Disabled Function
 # cd /usr/local/directadmin/custombuild; ./build update; ./build secure_php
 
-echo "Email : "
-read email
 
-
-sed 's/email@domain.com/$email/g' report.txt
+read -p 'Email : ' email
+sed 's/email@domain.com/$email/g' /usr/local/directadmin/custombuild/options.conf
 
 # Regular updates of system services, libraries and scripts.
 #cd /usr/local/directadmin/custombuild; ./build update; ./build all d
